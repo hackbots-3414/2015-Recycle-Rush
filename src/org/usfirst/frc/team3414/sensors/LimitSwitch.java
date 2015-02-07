0d65ba7189cc046e4ec00e04f50476775ecf86a6
Kayleigh Francis <kayleighrf@gmail.com>
2015-02-07 10:40:15 -0500

    Limit Switch has been created.

--- src/org/usfirst/frc/team3414/sensors/LimitSwitch.java
+++ src/org/usfirst/frc/team3414/sensors/LimitSwitch.java
@@ -1,37 +1,22 @@
 package org.usfirst.frc.team3414.sensors;
-import main.java.model.autonomous.SwitchPositions;
+import org.usfirst.frc.team3414.autonomous.SwitchPositions;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
- * 
  * The class responsible for controlling limit switches on digital IO ports. 
- * 
- * @generated
  */
-
 public class LimitSwitch implements ISwitch
 {
 	DigitalInput limitSwitch;
-	
-	/**
-	 * 
-	 * A constructor for the limit switch. It's arguments are the digital IO port that it's connected to on the RoboRIO.
-	 * 
-	 * @generated
-	 */
-	public LimitSwitch(int channel){
+
+	public LimitSwitch(int channel)
+	{
 		limitSwitch = new DigitalInput(channel);
 	}
 
-	/**
-	 * 
-	 * Gets the position of the limit switch, weather it's depressed (ON) or released (OFF).
-	 * 
-	 * @generated
-	 * @ordered
-	 */
-	
-	public SwitchPositions getPosition() {
+
+	public SwitchPositions getMode()
+	{
 		if (limitSwitch.get())
 		{
 			return SwitchPositions.ON;
