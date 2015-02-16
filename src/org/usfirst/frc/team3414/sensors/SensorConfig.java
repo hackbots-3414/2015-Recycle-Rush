a68d8dcc6fe9ba0738c51e18500f8cb0b4019972
Hackbots <yalamanchili.rahuly@gmail.com>
2015-02-16 11:38:57 -0500

    Adding SesorConfiguation

---
+++ src/org/usfirst/frc/team3414/sensors/SensorConfig.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team3414.sensors;
+
+public class SensorConfig {
+	
+	IClock clock;
+	IDistanceEventHandler distanceEventHandler;
+	IDetectLines lineDetector;
+	IMeasureAcceleration accelerometer;
+	
+}
