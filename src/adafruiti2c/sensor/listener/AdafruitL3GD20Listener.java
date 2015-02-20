3e3a3af73aa3e45fc5ff1138b0326604169f9506
Y-Rahul <yalamanchili.rahuly@gmal.com>
2015-02-19 19:44:28 -0500

    Initial import of Adafruit library for use in the 9DofF board.

---
+++ src/adafruiti2c/sensor/listener/AdafruitL3GD20Listener.java
@@ -0,0 +1,14 @@
+package adafruiti2c.sensor.listener;
+
+import java.util.EventListener;
+
+public abstract class AdafruitL3GD20Listener implements EventListener
+{
+	public void motionDetected(double x, double y, double z)
+	{
+	}
+
+	public void close()
+	{
+	}
+}
